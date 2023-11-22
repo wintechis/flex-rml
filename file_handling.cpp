0797ba42b10c0f1a7b49e63055e421be940aefd1
Michael Freund <michaelfreund95@gmail.com>
2023-11-22 09:05:10 +0100

    Remove unnecessary files

--- file_handling.cpp
+++
@@ -1,6 +0,0 @@
-#include "file_handling.h"
-
-// Read CSV data line by line
-void readCSV() {
-  std::ifstream file;
-}
--- file_handling.h
+++
@@ -1,6 +0,0 @@
-#ifndef FILE_HANDLING_H
-#define FILE_HANDLING_H
-
-#include <fstream>
-
-#endif
