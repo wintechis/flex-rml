c9fceffe63795560302a02a1798cc55e6bed6c79
Michael Freund <michaelfreund95@gmail.com>
2026-03-05 18:02:21 +0100

    Include external files

--- build_standalone.sh
+++ build_standalone.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-download_if_not_exists() {
-    local filepath=$1
-    local url=$2
-
-    if [ ! -f "$filepath" ]; then
-        echo "Downloading $filepath..."
-        wget -O "$filepath" "$url"
-    fi
-}
-
 check_if_exists() {
     local filepath=$1
     if [ ! -f "$filepath" ]; then
@@ -19,33 +9,6 @@ check_if_exists() {
         echo "Success."
     fi
 }
-
-echo "Preparing files ..."
-# Download RDF parser code (serd)
-download_if_not_exists ./frontend/rdf_parser/serd_lib/serd/serd.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/include/serd/serd.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/attributes.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/attributes.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/base64.c https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/base64.c
-download_if_not_exists ./frontend/rdf_parser/serd_lib/base64.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/base64.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/byte_sink.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/byte_sink.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/byte_source.c https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/byte_source.c
-download_if_not_exists ./frontend/rdf_parser/serd_lib/byte_source.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/byte_source.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/env.c https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/env.c
-download_if_not_exists ./frontend/rdf_parser/serd_lib/n3.c https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/n3.c
-download_if_not_exists ./frontend/rdf_parser/serd_lib/node.c https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/node.c
-download_if_not_exists ./frontend/rdf_parser/serd_lib/node.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/node.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/reader.c https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/reader.c
-download_if_not_exists ./frontend/rdf_parser/serd_lib/reader.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/reader.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/serd_config.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/serd_config.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/serd_internal.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/serd_internal.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/stack.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/stack.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/string.c https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/string.c
-download_if_not_exists ./frontend/rdf_parser/serd_lib/string_utils.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/string_utils.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/system.c https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/system.c
-download_if_not_exists ./frontend/rdf_parser/serd_lib/system.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/system.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/try.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/try.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/uri.c https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/uri.c
-download_if_not_exists ./frontend/rdf_parser/serd_lib/uri_utils.h https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/uri_utils.h
-download_if_not_exists ./frontend/rdf_parser/serd_lib/writer.c https://raw.githubusercontent.com/drobilla/serd/abfdac2085cba93bae76d3352f2ff7e40fdf1612/src/writer.c
 
 # Download Hash Function
 download_if_not_exists ./backend/executor/xxhash.h https://raw.githubusercontent.com/Cyan4973/xxHash/refs/heads/dev/xxhash.h
